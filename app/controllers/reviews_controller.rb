fb4933297ed160148e14c58af630a05644899466
Nate McEvoy <nate.omc@gmail.com>
2025-01-15 15:11:09 +0100

    small style change

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,32 +1,11 @@
-# class ReviewsController < ApplicationController
-#   def create
-#     @restaurant = Restaurant.find(params[:restaurant_id])
-#     @review = @restaurant.reviews.new(review_params)
-#     if @review.save
-#       redirect_to restaurant_path(@restaurant)
-#     else
-#       @reviews = @restaurant.reviews
-#       render 'restaurants/show', status: :unprocessable_entity
-#     end
-#   end
-
-#   private
-
-#   def review_params
-#     params.require(:review).permit(:content, :rating)
-#   end
-# end
-
 class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = @restaurant.reviews.new(review_params)
-
     if @review.save
-      redirect_to restaurant_path(@restaurant), notice: 'Review was successfully added.'
+      redirect_to restaurant_path(@restaurant)
     else
-      @reviews = @restaurant.reviews # Re-fetch the reviews after an invalid submission
-      flash.now[:alert] = 'Failed to add review. Please check the form for errors.'
+      @reviews = @restaurant.reviews
       render 'restaurants/show', status: :unprocessable_entity
     end
   end
@@ -37,3 +16,4 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:content, :rating)
   end
 end
+
